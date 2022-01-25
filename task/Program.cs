772ebf0ce3ae998acaa48393514dd4fcf50a32a8
Fostix <accretiondiskthat@gmail.com>
2022-01-25 13:19:23 +0500

    maked code

--- task/Program.cs
+++ task/Program.cs
@@ -5,14 +5,32 @@ int number = int.Parse(Console.ReadLine());
 if (number == 1)
 {
     Console.Write("Enter array length: ");
-    int lengthArray = int.Parse(Console.ReadLine());
-    Console.Write("Enter word(s) through space: ");
-    string[] array = new string[lengthArray];
+    int arrayLength = int.Parse(Console.ReadLine());
+    if (arrayLength > 0 | arrayLength < 21474833647)
+    {
+        string[] array = new string[arrayLength];
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("Enter word(s) through space: ");
+            array[i] = Console.ReadLine();
+        }
+        string[] newArray = Method(array);
+        Console.Write("Old array: ");
+        PrintArray(array);
+        Console.WriteLine();
+        Console.Write("New array: ");
+        PrintArray(newArray);
+    }
 }
+
 else if (number == 2)
 {
     string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
     string[] newArray = Method(array);
+    Console.Write("Old array: ");
+    PrintArray(array);
+    Console.WriteLine();
+    Console.Write("New array: ");
     PrintArray(newArray);
 }
 else
